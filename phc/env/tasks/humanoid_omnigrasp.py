63bf3cf884962fc01fa89036aa8164b3109ebb68
hongfz16 <hongfz16@163.com>
2025-04-01 10:51:49 +0800

    for pull

--- phc/env/tasks/humanoid_omnigrasp.py
+++ phc/env/tasks/humanoid_omnigrasp.py
@@ -1425,7 +1425,7 @@ def compute_grab_reward(root_pos, root_rot, obj_pos, obj_rot, obj_vel, obj_ang_v
     r_contact_lifted = contact_filter.float() 
 
     # # r_close = torch.exp(-k_pos * (hand_pos_diff.min(dim = -1).values **2))
-    contact_symbol = torch.where(torch.from_numpy(curr_contact_obs).to(r_contact_lifted.device) == 0, torch.zeros_like(r_contact_lifted) - 1, torch.zeros_like(r_contact_lifted) + 1)
+    contact_symbol = torch.where(torch.from_numpy(curr_contact_obs).to(r_contact_lifted.device) == 0, torch.zeros_like(r_contact_lifted) - 10, torch.zeros_like(r_contact_lifted) + 1)
     # print(contact_symbol, r_contact_lifted)
     # ##### pos_filter makes sure that no reward is given if the hand is too far from the object.
     # # reward = (w_pos * r_obj_pos + w_rot * r_obj_rot + w_vel * r_lin_vel + w_ang_vel * r_ang_vel) * contact_filter + r_contact_lifted * w_conctact  + r_close * w_close
