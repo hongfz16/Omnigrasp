fb90813863f45c6ba95f27c23879a5eafb0834c5
HongFangzhou <hongfz16@163.com>
2025-04-08 09:08:31 +0800

    for pull

--- fetch_asset.sh
+++ fetch_asset.sh
@@ -1,13 +1,11 @@
-# TtAsW^19980908
-
 set -x
 
-id=016
+id=$1
 
 tag=Scene-${id}
 scp wfcluster:/projects_vol/gp_slab/fangzhou_new/Multi-Object/${tag}/processed/omnigrasp/${tag}_sample_w_bg.pkl sample_data
 scp wfcluster:/projects_vol/gp_slab/fangzhou_new/Multi-Object/${tag}/processed/omnigrasp/${tag}.stl phc/data/assets/mesh/adt
 
-tag=scene_${id}
+# tag=scene_${id}
 mkdir -p output/HumanoidIm/omnigrasp_${tag}_track_15p_relative_rot
 scp wfcluster:/home/fangzhou.hong/Aria4D-scripts/submodules/Omnigrasp/output/HumanoidIm/omnigrasp_${tag}_track_15p_relative_rot/Humanoid.pth output/HumanoidIm/omnigrasp_${tag}_track_15p_relative_rot
